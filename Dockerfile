6ddc6c80bf7b26d4d777dea405dc91f67b5c21f0
Matthew Davidson <matthew.davidson@generalfusion.com>
2024-09-13 16:19:27 -0700

    first commit

--- Dockerfile
+++ Dockerfile
@@ -1,10 +1,10 @@
 FROM python:3
 
-WORKDIR /usr/src/app
-
-COPY requirements.txt ./
-RUN pip install --no-cache-dir -r requirements.txt
+WORKDIR /app
 
 COPY . .
+RUN python -m pip install --upgrade pip
+RUN pip install --no-cache-dir .
+
 
 CMD [ "python", "./main.py" ]
--- main.py
+++ main.py
@@ -1,1 +1,39 @@
-print("hello world from main.py")
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# ----------------------------------------------------------------------------
+# Created By  : Matthew Davidson
+# Created Date: 2023-01-23
+# version ='1.0'
+# ---------------------------------------------------------------------------
+"""a_short_module_description"""
+# ---------------------------------------------------------------------------
+
+import time
+from log_server import LogServer
+import logging
+
+HOST = "127.0.0.1"
+PORT = 9000
+
+logging.basicConfig(
+    level=logging.DEBUG,
+    format="%(asctime)s - %(name)s - %(levelname)s - %(message)s",
+)
+console = logging.StreamHandler()
+console.setLevel(logging.DEBUG)
+formatter = logging.Formatter("%(name)-12s: %(levelname)-8s %(message)s")
+console.setFormatter(formatter)
+logging.getLogger().addHandler(console)
+
+
+# Function to run the server
+def run_server():
+    log_server = LogServer(host=HOST, port=PORT, log_file="/var/log/log_server_0.log")
+    log_server.start()
+
+    while True:
+        time.sleep(1)
+
+
+if __name__ == "__main__":
+    run_server()
